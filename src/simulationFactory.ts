8fb65877e7f4185069336c0dea9b40e2139b5824
Florian Patzke <florian.patzke@t-online.de>
2024-10-21 10:26:47 +0200

    Refactor simulationFactory.ts to use separate classes for D3Simulation and LocalSimulation

--- src/simulationFactory.ts
+++ src/simulationFactory.ts
@@ -1,134 +1,117 @@
-import d3, {
-  forceSimulation,
-  forceManyBody,
-  forceCenter,
-  forceLink,
-  forceX,
-  forceY,
-} from "d3";
-
-
-/*
-      function createSimulation(parentNode) {
-        const radialForce = d3
-          .forceRadial(
-            0,
-            () => parentNode.x,
-            () => parentNode.y
-          )
-          .strength(0.5);
-        const simulation = d3
-          .forceSimulation()
-          .force(
-            "collision",
-            d3.forceCollide().radius((d) => d.r + padding)
-          )
-          .force("attraction", radialForce)
-          .on("tick", function () {
-            // Check if parent position has changed, if so add energy to the simulation
-            if (
-              lastPositions[parentNode.id] === undefined ||
-              lastPositions[parentNode.id].x !== parentNode.x ||
-              lastPositions[parentNode.id].y !== parentNode.y
-            ) {
-              simulation.alpha(0.3).restart();
-            }
-            lastPositions[parentNode.id] = { x: parentNode.x, y: parentNode.y };
-
-            // Update radial force center to follow the parent's current position
-            radialForce.radius(0).x(parentNode.x).y(parentNode.y);
-
-            // Directly update the nodes stored in simulation._nodes
-            simulation._nodes.forEach((d) => {
-              if (d.parent === parentNode.id) {
-                d3.select(`#node-${d.id}`).attr("cx", d.x).attr("cy", d.y);
-              }
-            });
-          });
-
-        simulation._nodes = []; // Store nodes internally
-
-        simulation.add = function (node) {
-          simulation._nodes.push(node);
-          simulation.nodes(simulation._nodes).alpha(1).restart();
-          updateParentNode(parentNode); // Update parent node position and radius
-        };
-
-        simulation.remove = function (node) {
-          simulation._nodes = simulation._nodes.filter((n) => n.id !== node.id);
-          simulation.nodes(simulation._nodes);
-          if (simulation._nodes.length === 0) {
-            simulation.stop();
-          }
-          updateParentNode(parentNode); // Update parent node position and radius
-        };
-
-        return simulation;
-      }
-*/
+import * as d3 from "d3";
 
 class D3Simulation {
-  private radialForce: any;
-  private simulationInstance: d3.Simulation<any, any>;
+  private simulation: d3.Simulation<any, any>;
+  public radialForce: d3.ForceRadial<any>;
 
-  constructor() {
-    this.radialForce = forceRadial(0, () => parentNode.x, () => parentNode.y).strength(0.5);
-    this.simulationInstance = forceSimulation()
-      .force("collision", forceCollide().radius((d: any) => d.r + padding))
-      .force("attraction", this.radialForce)
+  constructor(parentNode: any, padding: number) {
+    this.radialForce = d3
+      .forceRadial(
+        0,
+        () => parentNode.x,
+        () => parentNode.y
+      )
+      .strength(0.5);
+    this.simulation = d3
+      .forceSimulation()
+      .force(
+        "collision",
+        d3.forceCollide().radius((d: any) => d.r + padding)
+      )
+      .force("attraction", this.radialForce);
   }
 
-  public on = (type: string, callback: any) => {};
+  public on(
+    type: string,
+    callback: (this: d3.Simulation<any, any>, ...args: any[]) => void
+  ): this {
+    this.simulation.on(type, callback);
+    return this;
+  }
+
+  public nodes(nodes: any[]): this {
+    this.simulation.nodes(nodes);
+    return this;
+  }
+
+  public addNode(node: any): this {
+    const nodes = this.simulation.nodes();
+    nodes.push(node);
+    this.simulation.nodes(nodes);
+    return this;
+  }
+
+  public removeNode(node: any): this {
+    const nodes = this.simulation.nodes().filter((n) => n.id !== node.id);
+    this.simulation.nodes(nodes);
+    return this;
+  }
+
+  public restart(): this {
+    this.simulation.restart();
+    return this;
+  }
+
+  public stop(): this {
+    this.simulation.stop();
+    return this;
+  }
 }
 
 class LocalSimulation {
   private simulationInstance: any;
-  
-  private lastPositions: { [key: string]: { x: number; y: number } } = {};
   private subscribers: Set<HTMLElement> = new Set();
+  private parentNode: HTMLElement;
 
-  constructor(private parentNode: HTMLElement, simulation: D3Simulation = new D3Simulation()) {
+  constructor(
+    parentNode: HTMLElement,
+    simulation: D3Simulation = new D3Simulation(parentNode, 10)
+  ) {
+    this.parentNode = parentNode;
     this.simulationInstance = simulation;
     this.simulationInstance.on("tick", () => this.ticked());
   }
 
   private ticked() {
     const parentNode = this.parentNode;
-    if (
-      this.lastPositions[parentNode.id] === undefined ||
-      this.lastPositions[parentNode.id].x !== parentNode.x ||
-      this.lastPositions[parentNode.id].y !== parentNode.y
-    ) {
-      this.simulationInstance.alpha(0.3).restart();
-    }
-    this.lastPositions[parentNode.id] = { x: parentNode.x, y: parentNode.y };
+    const centerX = parentNode.offsetLeft + parentNode.offsetWidth / 2;
+    const centerY = parentNode.offsetTop + parentNode.offsetHeight / 2;
 
-    this.radialForce.radius(0).x(parentNode.x).y(parentNode.y);
-
-    this.simulationInstance._nodes.forEach((d: any) => {
-      if (d.parent === parentNode.id) {
-        d3.select(`#node-${d.id}`).attr("cx", d.x).attr("cy", d.y);
-      }
+    this.subscribers.forEach((node) => {
+      d3.select(node)
+        .style("left", (d: any) => `${d.x - d.r + centerX}px`)
+        .style("top", (d: any) => `${d.y - d.r + centerY}px`);
     });
   }
 
+  private updateSimulation = () => this.simulationInstance.nodes(Array.from(this.subscribers)).alpha(1).restart();
+
   public subscribe(node: any) {
     this.subscribers.add(node);
-    this.simulationInstance.nodes(this.simulationInstance._nodes).alpha(1).restart();
+    this.updateSimulation();
     this.updateParentNode(this.parentNode);
     return () => this.unsubscribe(node);
   }
 
   public unsubscribe(node: any) {
     this.subscribers.delete(node);
-    if (this.subscribers.size === 0) {
-      this.stop();
-    }
+    this.updateSimulation();
+    if (this.subscribers.size === 0) { this.stop(); }
     this.updateParentNode(this.parentNode);
   }
 
   private updateParentNode(parentNode: any) {
-    // Implement the logic to update the parent node position and radius
+    const nodes = Array.from(this.subscribers).map((node) => ({
+      x: node.offsetLeft + node.offsetWidth / 2,
+      y: node.offsetTop + node.offsetHeight / 2,
+      r: Math.max(node.offsetWidth, node.offsetHeight) / 2,
+    }));
+
+    const boundingCircle = d3.packEnclose(nodes);
+
+    parentNode.x = boundingCircle.x;
+    parentNode.y = boundingCircle.y;
+    parentNode.r = boundingCircle.r;
   }
 
   public stop() {
